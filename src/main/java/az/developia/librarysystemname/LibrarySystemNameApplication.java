e5b9eca648926d901e4ac8e84ff780ceaa22fb76
Orkhan93 <dokta228@mail.ru>
2023-08-26 18:53:18 +0400

    Added project structure and dependencies

---
+++ src/main/java/az/developia/librarysystemname/LibrarySystemNameApplication.java
@@ -0,0 +1,13 @@
+package az.developia.librarysystemname;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySystemNameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibrarySystemNameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/developia/librarysystemname/LibrarySystemNameApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.librarysystemname;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySystemNameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
